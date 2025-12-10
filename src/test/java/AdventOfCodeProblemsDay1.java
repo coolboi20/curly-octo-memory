7243eade856322b404e9ad9815d81d0568175a85
mmahase <mmahase@oldmutual.com>
2025-12-10 07:50:38 +0000

    Day 2, part 1 complete

---
+++ src/test/java/AdventOfCodeProblemsDay1.java
@@ -0,0 +1,141 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+public class AdventOfCodeProblemsDay1 {
+
+    /*
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+     */
+
+  @Test
+  void problem1Part1() throws Exception {
+    int[] array = new int[100];
+    int dial = 50;
+    int counter = 0;
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = i;
+    }
+
+//        System.out.println(Arrays.toString(array));
+
+    String[] instructions = {"L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"};
+
+    var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
+
+    for (String instruction : lines) {
+      char movement = instruction.charAt(0);
+      int value = Integer.parseInt(instruction.substring(1));
+
+      if (movement == 'L') {
+        int result = (dial - value) % array.length;
+        if (result < 0) {
+          dial = result + 100;
+        } else {
+          dial = result;
+        }
+      } else {
+        dial = (dial + value) % array.length;
+      }
+
+      if (dial == 0) {
+        counter++;
+      }
+
+    }
+
+    System.out.println("Password: " + counter);
+  }
+
+  @Test
+  void problem1Part2() throws IOException {
+    int passwordCounter = 0;
+    int dialPosition = 50;
+    int[] array = new int[100];
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = i;
+    }
+
+    var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
+
+//        for (var instructionAndValue : getPart1InputData()) {
+    for (var instructionAndValue : lines) {
+//            String instructionAndValue = "L100";
+      var instruction = instructionAndValue.charAt(0);
+      var value = Integer.parseInt(instructionAndValue.substring(1));
+
+      if (instruction == 'L') {
+        int remainder = (dialPosition - value) % array.length; // 0
+        int dividedValue = (dialPosition - value) / array.length; // -1
+        passwordCounter += (dividedValue < 0) ? -1 * dividedValue : dividedValue; // 1
+        if (remainder < 0) {
+          if (dialPosition != 0) {
+            passwordCounter++;
+          }
+          dialPosition = remainder + array.length;
+        } else if (remainder == 0) {
+          if (dialPosition != 0) {
+            passwordCounter++;
+          }
+          dialPosition = 0;
+        } else {
+          dialPosition = remainder;
+        }
+      } else {
+        int quotientWithoutDecimal = (dialPosition + value) / 100;
+        int remainder = (dialPosition + value) % 100;
+        passwordCounter += quotientWithoutDecimal;
+        if (remainder == 0 && quotientWithoutDecimal != 0) {
+//                    passwordCounter++;
+          dialPosition = remainder;
+        } else {
+          dialPosition = remainder;
+        }
+      }
+//            System.out.println("dial: " + dialPosition);
+    }
+    System.out.println("password: " + passwordCounter);
+  }
+
+  @Test
+  void freeThinking() {
+//        System.out.println(0 % 100); // -20
+    System.out.println((95 + 60) % 100); // -20   }
+  }
+
+  private String[] getPart1InputData() {
+    return new String[] {"L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"};
+  }
+
+
+}
